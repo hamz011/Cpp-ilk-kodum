3c57d66b7dbbc2fe2abd381227b3d0b6e5c123fc
hamz011 <hz0800908@gmail.com>
2024-05-06 21:19:00 +0300

    Add project files.

---
+++ Deneme1.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include <string>
+using namespace std;
+void HelloWorld(string a)
+{
+    string pp;
+    if (a == "print") {
+        pp = "Hello World";
+        cout << pp << endl;
+    }
+
+
+
+}
+int main()
+{
+   // HelloWorld("print");
+    int a = 11;
+    a = a + 10;
+    kare();
+   // cout << "A" + a << endl;
+    return 0;
+}
+void kare()
+{
+
+    int sayi1,sayi2,sayi3,toplam,ss;
+    std::cout << "1. sayıyı giriniz: ";
+    std::cin >> sayi1;
+    std::cout << "2. sayıyı giriniz: ";
+    std::cin >> sayi2 ;
+    std::cout << "3. sayıyı giriniz: ";
+    std::cin >> sayi3 ;
+
+    toplam=sayi1+sayi2+sayi3;
+    ss=toplam;
+    toplam *= toplam;
+
+    cout <<ss<<", karesi: "<<toplam;
+    system("PAUSE");
+
+}
